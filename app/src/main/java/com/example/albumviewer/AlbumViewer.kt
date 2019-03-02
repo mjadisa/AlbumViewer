4169cc709923b45e88c679a4d20b4e4a89c1bb3d
Mujeeb Adisa <user@example.com>
2019-03-02 14:26:11 +0000

    Implemented injection in application class and configured gradle files to enable kapt processing

--- app/src/main/java/com/example/albumviewer/AlbumViewer.kt
+++ app/src/main/java/com/example/albumviewer/AlbumViewer.kt
@@ -1,9 +1,20 @@
 package com.example.albumviewer
 
 import android.app.Application
+import com.example.albumviewer.di.AppComponent
+import com.example.albumviewer.di.DaggerAppComponent
 
 class AlbumViewer : Application() {
+
+
+    private val appComponent: AppComponent by lazy {
+        DaggerAppComponent.builder()
+            .application(this)
+            .build()
+
+    }
     override fun onCreate() {
         super.onCreate()
+        appComponent.inject(this)
     }
 }
--- app/src/main/java/com/example/albumviewer/db/AlbumViewerDatabase.kt
+++ app/src/main/java/com/example/albumviewer/db/AlbumViewerDatabase.kt
@@ -3,8 +3,9 @@ package com.example.albumviewer.db
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 import com.example.albumviewer.common.DATABASE_VERSION
+import com.example.albumviewer.data.Album
 
-@Database(entities = [AlbumsDao::class], version = DATABASE_VERSION)
+@Database(entities = [Album::class], version = DATABASE_VERSION)
 abstract class AlbumViewerDatabase : RoomDatabase() {
     abstract fun albumsDao(): AlbumsDao
 }
