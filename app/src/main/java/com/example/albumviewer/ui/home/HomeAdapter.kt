48617f189984f87138ecbf11603bb3bb0baaf62c
Mujeeb Adisa <user@example.com>
2019-03-02 15:55:41 +0000

    Implemented Recyclerview Adapter

---
+++ app/src/main/java/com/example/albumviewer/ui/home/HomeAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.albumviewer.ui.home
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.albumviewer.data.Album
+import com.example.albumviewer.databinding.ItemAlbumBinding
+import kotlinx.android.extensions.LayoutContainer
+
+class HomeAdapter : RecyclerView.Adapter<HomeAdapter.AlbumViewHolder>() {
+
+    private val data = mutableListOf<Album>()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): AlbumViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return AlbumViewHolder(ItemAlbumBinding.inflate(layoutInflater, parent, false))
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(viewHolder: AlbumViewHolder, position: Int) {
+        viewHolder.bind(data[position])
+    }
+
+    inner class AlbumViewHolder(val itemBinding: ItemAlbumBinding) : RecyclerView.ViewHolder(itemBinding.root),
+        LayoutContainer {
+        override val containerView: View?
+            get() = itemBinding.root
+
+        fun bind(album: Album) {
+            itemBinding.album = album
+            itemBinding.executePendingBindings()
+        }
+    }
+}
