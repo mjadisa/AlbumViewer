7577a1b170049c464c7abe368c2dffb00bed07cd
Mujeeb Adisa <user@example.com>
2019-03-03 12:26:51 +0000

    Added test for AlbumRepository

---
+++ app/src/test/java/com/example/albumviewer/AlbumsRepositoryTest.kt
@@ -0,0 +1,63 @@
+package com.example.albumviewer
+
+import com.example.albumviewer.data.Album
+import com.example.albumviewer.repo.AlbumsRepository
+import com.example.albumviewer.repo.DataSource
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import io.reactivex.Maybe
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.anyBoolean
+import org.mockito.Mockito.times
+import org.mockito.junit.MockitoJUnitRunner
+import java.io.IOException
+
+@RunWith(MockitoJUnitRunner::class)
+class AlbumsRepositoryTest {
+    private val localDataSource: DataSource = mock()
+
+    private val remoteDataSource: DataSource = mock()
+
+    private lateinit var albumsRepository: DataSource
+    private val albums = mutableListOf<Album>()
+    private val album = Album(0, 0, 1, "Test Album")
+
+    @Before
+    fun setup() {
+        albumsRepository = AlbumsRepository(localDataSource, remoteDataSource)
+        albums.add(album)
+    }
+
+    @Test
+    fun testDataRetrievalFromApi() {
+        //Given
+        whenever(remoteDataSource.getSortedAlbums(anyBoolean())).thenReturn(Maybe.just(albums))
+        whenever(localDataSource.getSortedAlbums(anyBoolean())).thenReturn(Maybe.empty())
+        //When
+        val testObserver = albumsRepository.getSortedAlbums(true).test()
+
+        //Then
+        verify(remoteDataSource).getSortedAlbums(true)
+        verify(localDataSource).addAlbum(album)
+        testObserver.assertValueCount(1)
+
+    }
+
+    @Test
+    fun testDataRetrievalFromDb() {
+        //Given
+        whenever(remoteDataSource.getSortedAlbums(anyBoolean())).thenReturn(Maybe.error(IOException()))
+        whenever(localDataSource.getSortedAlbums(anyBoolean())).thenReturn(Maybe.just(albums))
+        //When
+        val testObserver = albumsRepository.getSortedAlbums(true).test()
+
+        //Then
+        verify(remoteDataSource).getSortedAlbums(true)
+        verify(localDataSource).getSortedAlbums(true)
+        verify(localDataSource, times(0)).addAlbum(album)
+        testObserver.assertValueCount(1)
+    }
+}
